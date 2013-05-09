af319596d417a9021cf1f588d8d00607c9689898
Anders Ullnaess <andersu@mesan.no>
2013-05-09 15:44:50 +0200

    Tester geolocation

--- assets/www/main.js
+++ assets/www/main.js
@@ -30,6 +30,14 @@ var deviceInfo = function() {
 var getLocation = function() {
     var suc = function(p) {
         alert(p.coords.latitude + " " + p.coords.longitude);
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
     };
     var locFail = function() {
     };
