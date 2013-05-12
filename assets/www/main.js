27c3b3810d64a55ce06c1a1161ebd8df4beadc56
ToreB <toreb@mesan.no>
2013-05-13 00:12:40 +0200

    glemte noen filer

--- assets/www/main.js
+++ assets/www/main.js
@@ -28,15 +28,8 @@ var deviceInfo = function() {
 };
 
 var getLocation = function() {
-    var suc = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+    var suc = function(p) {
+        alert(p.coords.latitude + " " + p.coords.longitude + " " + p.coords.altitude);
     };
     var locFail = function() {
     };
